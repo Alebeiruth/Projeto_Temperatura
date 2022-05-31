5b0f05f33611b071352a3aa4cf4742374e6ac7b4
Alexandre Beiruth <101944430+Alebeiruth@users.noreply.github.com>
2022-05-31 20:57:47 -0300

    Update projeto_temp.js

--- projeto_temp.js
+++ projeto_temp.js
@@ -1,10 +1,10 @@
 //projetos de termometro 
 const kelvin = 293;
 
-// diferença de kelvin para celsius 273
+//diferença de kelvin para celsius 273
 const celsius = kelvin - 273;
 
-// calculo de fahrenheit
+//calculo de fahrenheit
 var fahrenheit = celsius * (9 / 5) + 32;
 
 //converter Celsius para Newton
@@ -12,7 +12,7 @@ var Newton = celsius * (33 / 100)
 Newton = Math.floor(Newton)
 console.log(`The temperature is ${Newton} degress Newton.`);
 
-// vamos arredondar
+//vamos arredondar
 fahrenheint = Math.floor(fahrenheit);
 console.log(`The tamperature is ${fahrenheit} degrees Fahrenheit.`);
 
